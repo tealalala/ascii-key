162d9f4ae278b0ead626dc48c36ce246da9f8726
Tea <tealalala@protonmail.com>
2019-05-13 12:41:22 -0700

    Behavior: Add keypress output to DOM

--- js/main.js
+++ js/main.js
@@ -1,5 +1,9 @@
 window.addEventListener("keypress", printKeypress);
 
 function printKeypress(e) {
-  console.log(e);
+  // console.log(e);
+  // console.log(e.key);
+  var pressedKey = e.key;
+  var outputPressedKey = document.querySelector(".letter");
+  outputPressedKey.innerHTML = pressedKey;
 };

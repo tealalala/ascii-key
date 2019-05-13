62b195f35cdbe655700b2e442c7cacb095edd296
Tea <tealalala@protonmail.com>
2019-05-13 12:53:38 -0700

    Behavior: Add decimal code and hexcode outputs to DOM

--- js/main.js
+++ js/main.js
@@ -1,9 +1,29 @@
 window.addEventListener("keypress", printKeypress);
 
 function printKeypress(e) {
-  // console.log(e);
+  console.log(e);
   // console.log(e.key);
-  var pressedKey = e.key;
-  var outputPressedKey = document.querySelector(".letter");
+  let pressedKey = e.key;
+  let pressedKeyCode = e.keyCode;
+  let pressedKeyDecimal = e.charCode;
+
+  // KeyOutput
+  let outputPressedKey = document.querySelector(".letter");
   outputPressedKey.innerHTML = pressedKey;
+
+  // Keycode Output
+  let outputPressedKeyCode = document.querySelector(".keycode");
+  outputPressedKeyCode.innerHTML = pressedKeyCode;
+
+  // Decimal Code Output
+  let outputPressedKeyDecimal = document.querySelector(".decimal-code");
+  outputPressedKeyDecimal.innerHTML = pressedKeyDecimal;
+
+  // Hexcode Output
+  printHexcode(e);
+};
+
+function printHexcode(e) {
+  let outputPressedKeyHexcode = document.querySelector(".hexcode");
+  outputPressedKeyHexcode.innerHTML = e.charCode - 36;
 };

6f1acfe87496796efb4fc4442315d85a041157e7
Tea <tealalala@protonmail.com>
2019-05-14 11:59:33 -0700

    Behavior: Add history limit to 10 printed to DOM

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 window.addEventListener("keypress", printKeypress);
 
+var historyArr = [];
+console.log(`Before: ${historyArr}`);
+
 function printKeypress(e) {
   console.log(e);
   // console.log(e.key);
@@ -21,9 +24,36 @@ function printKeypress(e) {
 
   // Hexcode Output
   printHexcode(e);
+
+  // Print History to DOM
+  printHistoryToDOM(e);
 };
 
 function printHexcode(e) {
   let outputPressedKeyHexcode = document.querySelector(".hexcode");
   outputPressedKeyHexcode.innerHTML = e.keyCode.toString(16);
+};
+
+function printHistoryToDOM(e) {
+  historyArr.push(e.key);
+  if (historyArr.length > 10) {
+    historyArr.shift();
+    var elRemove = document.querySelector("span");
+    var parent = elRemove.parentNode.removeChild(elRemove);
+    console.log(`elRemove: ${elRemove}`);
+    console.log(`parentNode: ${parent}`);
+    console.log(`historyArr: ${historyArr}`);
+  }
+
+  var el = document.querySelector(".history-items");
+  var newEl = document.createElement("span");
+  var createEl = document.createTextNode(e.key);
+  newEl.appendChild(createEl);
+  el.appendChild(newEl);
+
+  console.log(el);
+  console.log(newEl);
+  console.log(createEl);
+  
+  console.log(`After: ${historyArr}`);
 };

e0ad154d7da16be18c50c94a9d7e1e37c066ddaa
Tea <tealalala@protonmail.com>
2019-05-15 17:37:14 -0700

    Refactor: submitASCIItoTable function

--- js/main.js
+++ js/main.js
@@ -1,11 +1,21 @@
+/* Table of Contents
+ * 1 - Keypress
+ *    1-1 - Print Keypresses to DOM
+ *    1-2 - Print Keypress History to DOM
+ * 2 - Down Arrow
+ * 3 - Form
+*/
+
+///////////////////////////////////////////////////////////////
+
+// 1 - Keypress
+
+// 1-1 - Print Keypresses to DOM
 window.addEventListener("keypress", printKeypress);
 
 var historyArr = [];
-// console.log(`Before: ${historyArr}`);
 
 function printKeypress(e) {
-  // console.log(e);
-  // console.log(e.key);
   let pressedKey = e.key;
   let pressedKeyCode = e.keyCode;
   let pressedKeyDecimal = e.charCode;
@@ -24,62 +34,59 @@ function printKeypress(e) {
       function printPressedKeyCode(e) {
         let outputPressedKeyCode = document.querySelector(".keycode");
         outputPressedKeyCode.innerHTML = e.keyCode;
-      }
+      };
 
       function printDecimalCode(e) {
         let outputPressedKeyDecimal = document.querySelector(".decimal-code");
         outputPressedKeyDecimal.innerHTML = e.charCode;
-      }
+      };
 
       function printHexcode(e) {
         let outputPressedKeyHexcode = document.querySelector(".hexcode");
         outputPressedKeyHexcode.innerHTML = e.keyCode.toString(16);
       };
 
+
+
+// 1-2 - Print Keypress History to DOM
 function printHistoryToDOM(e) {
   historyArr.push(e.key);
   if (historyArr.length > 10) {
-    // historyArr.shift();
     historyArr.shift();
-    var elRemove = document.querySelector("span");
-    var parent = elRemove.parentNode.removeChild(elRemove);
-    // console.log(`elRemove: ${elRemove}`);
-    // console.log(`parentNode: ${parent}`);
-    // console.log(`historyArr: ${historyArr}`);
-  }
+    let elRemove = document.querySelector("span");
+    let parent = elRemove.parentNode.removeChild(elRemove);
+  };
 
-  var el = document.querySelector(".history-items");
-  var newEl = document.createElement("span");
-  var createEl = document.createTextNode(e.key);
+  let el = document.querySelector(".history-items");
+  let newEl = document.createElement("span");
+  let createEl = document.createTextNode(e.key);
   newEl.appendChild(createEl);
   el.appendChild(newEl);
-
-  // console.log(el);
-  // console.log(newEl);
-  // console.log(createEl);
-  //
-  // console.log(`After: ${historyArr}`);
 };
 
 
-/////////////////
 
+///////////////////////////////////////////////////////////////
+
+// 2 - Down Arrow
 document.querySelector(".proceed-to-form-arrow").addEventListener("click", downArrowIsClicked);
 
 function downArrowIsClicked(e) {
   location.href = "#ascii-form";
 };
 
+///////////////////////////////////////////////////////////////
 
-////////////////
 
+
+// 3 - Form
 document.querySelector("#submit-form").addEventListener("click", submitASCIItoTable);
 
 function submitASCIItoTable(e) {
     console.log("successful click: submit-form");
     console.log(e);
-    var checkValue = document.querySelector(".ascii-input").value;
-    console.log(checkValue);
+    var inputValue = document.querySelector(".ascii-input").value;
+    console.log(inputValue);
 
     var table = document.querySelector(".table");
     var row = table.insertRow(-1);
@@ -87,8 +94,19 @@ function submitASCIItoTable(e) {
     var cell2 = row.insertCell(1);
     var cell3 = row.insertCell(2);
     var cell4 = row.insertCell(3);
-    cell1.innerHTML += "1";
-    cell2.innerHTML += "1";
-    cell3.innerHTML += "1";
-    cell4.innerHTML += "1";
+    cell1.innerHTML += printPressedKeyToTable(inputValue);
+    cell2.innerHTML += printKeyCodeToTable(inputValue);
+    cell3.innerHTML += printKeyCodeToTable(inputValue);
+    cell4.innerHTML += printHexcodeToTable(inputValue);
 };
+      function printPressedKeyToTable(e) {
+        return e;
+      };
+
+      function printKeyCodeToTable(e) {
+        return e.charCodeAt(0);
+      };
+
+      function printHexcodeToTable(e) {
+        return e.charCodeAt(0).toString(16);
+      };
